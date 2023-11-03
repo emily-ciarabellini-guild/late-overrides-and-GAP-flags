d082222267b47dc5e490f906baad873a64d3ab4f
Emily Ciarabellini <114701693+emily-ciarabellini-guild@users.noreply.github.com>
2023-11-03 16:27:38 -0700

    Add working, fast script for GAP and overrides

--- late-overrides-with-snowflake.py
+++ late-overrides-with-snowflake.py
@@ -2,8 +2,10 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 import snowflake.connector
+from snowflake.connector import DictCursor
 import csv
 import datetime
+import pandas as pd
 
 
 dotenv_path = join(dirname(__file__),'.env')
@@ -64,6 +66,24 @@ SQL_STUDENT_TERM_LINE_ITEMS_STUDENT_ID = connection.execute_string(
     """
     )
 
+SQL_INVOICE_MGMT = connection.execute_string(
+    """
+    SELECT DISTINCT CONCAT(TLI.TERM_CODE,'_', SLI.STUDENT_EXTERNAL_ID) AS KEY, MAX(I.UPDATED_AT)
+    FROM TA_ORCHESTRATOR_PUBLIC.TERM_LINE_ITEMS TLI
+    JOIN TA_ORCHESTRATOR_PUBLIC.STUDENT_LINE_ITEMS SLI ON SLI.ID = TLI.STUDENT_LINE_ITEM_ID
+    JOIN TA_ORCHESTRATOR_PUBLIC.INVOICES I ON I.ID = SLI.INVOICE_ID
+    WHERE I.STATE = 'COMMITTED'
+    GROUP BY KEY
+    """
+    )
+
+SQL_TA1 = connection.execute_string(
+    """
+    SELECT DISTINCT CONCAT(TERM_CODE,'_', PARTNER_STUDENT_ID) AS KEY, MAX(BENEFIT_LOCKED_ON)
+    FROM TA_ORCHESTRATOR_PUBLIC.LEGACY_SALESFORCE_LINE_ITEMS SFLI
+    GROUP BY KEY
+    """
+    )
 
 SQL_TUITION_ELIGIBILITY_OVERRIDES = connection.execute_string(
     """
@@ -101,19 +121,27 @@ for x in SQL_STUDENT_TERM_LINE_ITEMS_UUID:
       stli.extend([row[0], row[1]])
       mlbSTLIs_UUID.append(stli)
 
-mlbSTLIs_StudentID = [['KEY', 'MAX(UPDATED_AT)']]
-for x in SQL_STUDENT_TERM_LINE_ITEMS_STUDENT_ID:
-   for row in x:
-      stli = []
-      stli.extend([row[0], row[1]])
-      mlbSTLIs_StudentID.append(stli)
- 
 tuitionOverrides = [['KEY', 'UPDATED_AT', 'OVERRIDE_LOGGED_BY', 'AP_NAME', 'REASON', 'TUITION_ELIGIBLE', 'STUDENT_EXTERNAL_ID', 'TERM_CODE', 'COMMENT', 'MP SEARCH URL' ]]
 for x in SQL_TUITION_ELIGIBILITY_OVERRIDES:
     for row in x:
         override = []
         override.extend([row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9]])  # turns row into a list
         tuitionOverrides.append(override)
+
+
+def createDictfromCursor(cursor):
+    """
+    Takes a Snowflake Cursor object as an arguments and returns a dictionary. 
+    The Snowflake Cursor object contains a nested list. In the resulting
+    dictionary, the first value of a nested list will be the key and the second 
+     will be the value.
+    ex: {"key": "timestamp"}
+    """ 
+    dict = {"key": "timestamp"}
+    for x in cursor:
+        for row in x:
+            dict[row[0]] =  row[1]
+    return dict
 
 
 def createListfromCSV(csvFileName):
@@ -126,21 +154,20 @@ def createListfromCSV(csvFileName):
     return new_list
 
 
-# def combineLists(list1, list2):
-#     """
-#     Combines two lists where the values are dates. This function
-#     maintains the value where the date is the most recent for a given key 
-#     when there are duplicate keys across the two dictionaries. 
-#     """ 
-#     newList = list1 | list2
-#     for k in newList:
-#         if k in list1 and k in list2:
-#             if list1[k] > list2[k]:
-#                 newList[k] = list1[k]
-#             else:
-#                 newList[k] = list2[k]
-#     return newList
-
+def combineDicts(dict1, dict2):
+    """
+    Combines two dictionaries where the values are dates. This function
+    maintains the value where the date is the most recent for a given key 
+    when there are duplicate keys across the two dictionaries. 
+    """ 
+    newDict = dict1 | dict2
+    for k in newDict:
+        if k in dict1 and k in dict2:
+            if dict1[k] > dict2[k]:
+                newDict[k] = dict1[k]
+            else:
+                newDict[k] = dict2[k]
+    return newDict
 
 
 def excludePermissables(permissables,overrides):
@@ -174,6 +201,28 @@ def lateOverrideCheck(overrides, lines):
     return lateOverrideslist
 
 
+def lateOverrideCheckWdict(overrides, linesDict):
+    """
+    Compares date of logged overrides to the date of the committed line item for the
+    given term_studentID key. If the override is logged after the line item was 
+    committed, the term_studentID key is added to the late override list. This function
+    returns a list of the late override list and the overrides not found.
+    Overrides is a list with termcode_studentID in column 1 and a date in 
+    column 2. LinesDict is a dictionary with key:value pairs, ex: 
+    {"key": "timestamp", "key": "timestamp", "key": "timestamp"}
+    """ 
+    lateOverrideslist = []
+    overridesWithNoLoggedSTLIs = []
+
+    for ovrd in overrides:
+        if ovrd[0] in linesDict:
+            if ovrd[1] > linesDict[ovrd[0]]:
+                lateOverrideslist.append(ovrd)
+        else:
+            overridesWithNoLoggedSTLIs.append(ovrd)
+    return [lateOverrideslist,overridesWithNoLoggedSTLIs]
+
+
 def writeToCSV(list,filename):
     """
     Takes a list and a CSV file name as parameters and writes the contents of the 
@@ -185,27 +234,23 @@ def writeToCSV(list,filename):
         wrapper.writerow(i)
     file.close()
 
+mlbSTLIs_SID_dict = createDictfromCursor(SQL_STUDENT_TERM_LINE_ITEMS_STUDENT_ID)
+InvMgmt_dict = createDictfromCursor(SQL_INVOICE_MGMT)
+TA1_dict = createDictfromCursor(SQL_TA1)
+ta1_imDict = combineDicts(TA1_dict,InvMgmt_dict)
+allLinesDict = combineDicts(ta1_imDict,mlbSTLIs_SID_dict)
+
+gapResult = lateOverrideCheck(gapFlags,mlbSTLIs_UUID)  #  GAP flags do not contain UUID, so this check is based on UUID ............... need to see if can pull in TA 1.0 and IM data with UUID...............
+gapHeader = gapFlags[0]
+gapResult.insert(0,gapHeader)
+writeToCSV(gapResult,'_gapResults.csv')
+print("Count of late GAP flags is: ", len(gapResult)-1)
+
 permissables = createListfromCSV('Permissables.csv')
-ta1List = createListfromCSV('TA1.csv')  #Ideally pull this TA1.0 and IM data from snowflake as well? .......................................................................................................................................................
-imList = createListfromCSV('invoicemanagement_lines.csv') #Ideally pull this TA1.0 and IM data from snowflake as well? ....................................................................................................................................
-
-
-
-# gapResult = lateOverrideCheck(gapFlags,mlbSTLIs_UUID)  # Need to change this to also review TA 1.0 and IM line items. make sure not to exclude permissables for GAP.........................................................................................
-# gapHeader = gapFlags[0]
-# gapResult.insert(0,gapHeader)
-# writeToCSV(gapResult,'_gapResults.csv')
-# print("Count of late GAP flags is: ", len(gapResult)-1)
-
 overridesMinusPermissables = excludePermissables(permissables,tuitionOverrides)
-overridesResult1 = lateOverrideCheck(overridesMinusPermissables, mlbSTLIs_StudentID)
+overridesResult = lateOverrideCheckWdict(overridesMinusPermissables, allLinesDict)
+lateOverrides = overridesResult[0]
 overrideHeader = tuitionOverrides[0]
-overridesResult1.insert(0,overrideHeader)
-writeToCSV(overridesResult1,'_overridesResults.csv')
-print("Count of late overrides is: ", len(overridesResult1)-1)
-##need to incorporate 1.0, Invoice Management, and Permissibles in the late override
-
-# x=0
-# while x < 10:
-#     print(ta1List[x])
-#     x+=1
+lateOverrides.insert(0,overrideHeader)
+writeToCSV(lateOverrides,'_lateOverridesResults.csv')
+print("Count of late overrides is: ", len(lateOverrides)-1)
